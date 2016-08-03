6c4d4685c3a5c6f6eeedd13bfe25c3bba788e161
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-03 15:04:35 -0400

    segment different parts, regExp string basic function, replace letters with underscores in word

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 "use strict";
 
 $(document).ready(function() {
-	console.log("ready");
+	var Street = function(name) {
+		this.name = name;
+		this.name_length = this.name.length;
+	};
+
+	var mccaul = new Street("McCaul");
+	var user_guess = "M";
+
+	$(".correct_letters").children("h2")
+		.text(function() {
+			var name_as_underscores = "";
+			for (var i = 0; i < mccaul.name_length; i++) {
+				name_as_underscores += "_ ";
+			};
+			return name_as_underscores;
+		});
+
+	console.log(mccaul.name.search(user_guess));
+
 })
