156c92a1d0c0dcd5e3fd036958d1a17475bdbd0b
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-03 16:07:44 -0400

    console.log tests on regExp iterations

--- js/main.js
+++ js/main.js
@@ -5,19 +5,45 @@ $(document).ready(function() {
 		this.name = name;
 		this.name_length = this.name.length;
 	};
+	var User_guess = function(input) {
+		this.guess = new RegExp(input + "+", "gi");
+	}
 
 	var mccaul = new Street("McCaul");
-	var user_guess = "M";
+	var user1 = new User_guess("c");
 
+	var name_as_underscores = "";
+
+
+	// turn Street.name letters to underscores
 	$(".correct_letters").children("h2")
 		.text(function() {
-			var name_as_underscores = "";
 			for (var i = 0; i < mccaul.name_length; i++) {
-				name_as_underscores += "_ ";
+				name_as_underscores += "_";
 			};
 			return name_as_underscores;
 		});
 
-	console.log(mccaul.name.search(user_guess));
+	// Check if User.guess is found within Street.name
+	if (mccaul.name.match(user1.guess) !== null) {
+		console.log("CORRECT guess!");
+	}	else {
+		console.log("INCORRECT guess!")
+	}
+	
+	// This returns an array
+	// => ["cC", index: 1, input: "McCaul"]
+	console.log(user1.guess.exec(mccaul.name));
 
 })
+
+
+
+
+
+
+
+
+
+
+
